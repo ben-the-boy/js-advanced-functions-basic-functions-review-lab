392c96761e859202f56a9b0239e3905f62a337da
Ben Hayes <b.t.hayes@comcast.net>
2021-07-08 16:42:44 -0500

    commit tree 919414590c8ac58b70dac8f949f1e852d7e01a6c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(input = "*") {
+  return function(adj = "special") {
+    return `You are ${input}${adj}${input}!`
+  }
+}
+
+const Calculator = {
+  add: function(num1, num2) {
+    return num1 + num2;
+  },
+  subtract: function(num1, num2) {
+    return num1 - num2;
+  },
+  multiply: function(num1, num2) {
+    return num1 * num2;
+  },
+  divide: function(num1, num2) {
+    return num1 / num2;
+  }
+}
+
+function actionApplyer(int, arr) {
+  let base = int
+  for (let i = 0; i < arr.length; i++) {
+    base = arr[i](base);
+  }
+  return base
+}
